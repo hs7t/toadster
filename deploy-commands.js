d0230a9c1892ace49a74b4f23e085bd7a24c697e
Ali <125828096+hs7t@users.noreply.github.com>
2025-07-05 10:34:58 -0500

    implement command deployment

---
+++ deploy-commands.js
@@ -0,0 +1,24 @@
+const { REST, Routes } = require('discord.js');
+const readCommands = require('./utilities/readCommands.js');
+const { clientId, guildId, token } = require('./identity.json');
+const path = require('node:path');
+
+const commandsFolderPath = path.join(__dirname, 'commands');
+const commands = readCommands(commandsFolderPath);
+
+const rest = new REST().setToken(token);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
