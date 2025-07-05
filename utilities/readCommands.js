6284b2fef477d120d3ccc48b9a2e224ac408201c
Ali <125828096+hs7t@users.noreply.github.com>
2025-07-05 10:34:48 -0500

    add basic commands

---
+++ utilities/readCommands.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+function readCommands(commandsFolderPath) {
+    let commands = [];
+    
+    const commandFolders = fs.readdirSync(commandsFolderPath);
+
+    for (const folder of commandFolders) {
+        const commandsPath = path.join(commandsFolderPath, folder);
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath);
+            if ('data' in command && 'execute' in command) {
+                commands.push(command.data.toJSON());
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
+        }
+    }
+    
+    return commands;
+}
+
+module.exports = readCommands;
