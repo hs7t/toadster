0e6fa6dd48103634eb48de55b209716c65364c6a
Ali <125828096+hs7t@users.noreply.github.com>
2025-07-13 16:44:38 -0500

    fix

--- commands/personality/pen.js
+++ commands/personality/pen.js
@@ -1,10 +1,71 @@
 const { SlashCommandBuilder } = require('discord.js')
-const handleCommand = require('../../complex/pen/commandHandler.js')
+
+const {
+    ActionRowBuilder,
+    Events,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+} = require('discord.js')
+
+const Sentiment = require('sentiment')
+
+const LETTER_CONTENT_INPUT_ID = 'letterContentInput'
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('pen')
         .setDescription('lets you pen a letter!'),
     async execute(interaction) {
-        handleCommand(interaction)
+        var letter = {}
+        const modal = createModal()
+
+        await interaction.showModal(modal)
+
+        const filter = (i) =>
+            i.customId === 'letterContent' && i.user.id === interaction.user.id
+
+        interaction
+            .awaitModalSubmit({ filter, time: 120_000 })
+            .then(async (submission) => {
+                await submission.deferReply()
+                letter.content = submission.fields.getTextInputValue(
+                    LETTER_CONTENT_INPUT_ID,
+                )
+                const sentiment = new Sentiment()
+                letter.rating = sentiment.analyze(letter.content)
+
+                submission.followUp({
+                    content: 'ribeep! letter received',
+                    ephemeral: true,
+                })
+            })
+            .catch((error) => {
+                console.error(`something bad happened: ${error}`)
+                interaction.followUp({
+                    content: "oh no! you didn't send your letter in time.",
+                    ephemeral: true,
+                })
+            })
     },
+}
+
+function createModal() {
+    const modal = new ModalBuilder()
+        .setCustomId('letterContent')
+        .setTitle('Letter to toadster')
+
+    const letterContentInput = new TextInputBuilder()
+        .setCustomId(LETTER_CONTENT_INPUT_ID)
+        .setLabel('Write a letter...')
+        .setStyle(TextInputStyle.Paragraph)
+        .setPlaceholder('Dearest toad,')
+        .setMinLength(100)
+        .setRequired(true)
+
+    const actionRowA = new ActionRowBuilder().addComponents(letterContentInput)
+
+    modal.addComponents(actionRowA)
+
+    return modal
 }
