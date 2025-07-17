b49fb01451c738f34fe999c60a4166e6e610c8dd
Ali <125828096+hs7t@users.noreply.github.com>
2025-07-16 22:12:40 -0500

    add answer command

---
+++ commands/tools/answer.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder, Embed } = require('discord.js')
+const { EmbedBuilder } = require('discord.js')
+const secrets = require('../../secrets.json')
+
+async function getWolframShortAnswer(query) {
+    const url = `http://api.wolframalpha.com/v1/result?appid=${secrets.wolframalpha.shortanswers.key}&i=${encodeURIComponent(query)}&units=metric&timeout=10`
+
+    const response = await fetch(url)
+
+    if (!response.ok) {
+        console.error(
+            'API response status:',
+            response.status,
+            await response.text(),
+        )
+        throw new Error('oh no! error in response')
+    }
+
+    return await response.text()
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('answer')
+        .setDescription(
+            'gives you a short answer to a query using Wolfram Alpha!',
+        )
+        .addStringOption((option) =>
+            option
+                .setName('query')
+                .setDescription(
+                    'a query, like "distance from Bogotá to Lisboa"',
+                )
+                .setRequired(true),
+        ),
+
+    async execute(interaction) {
+        await interaction.deferReply()
+
+        const query = await interaction.options.getString('query')
+
+        let answer = undefined
+        try {
+            answer = await getWolframShortAnswer(query)
+        } catch (error) {
+            console.error(
+                'something went wrong in getWolframShortAnswer:',
+                error,
+            )
+            return
+        }
+
+        const responseEmbed = new EmbedBuilder()
+            .setTitle(answer)
+            .setDescription(`result for "${query}"`)
+            .setFooter({ text: 'from Wolfram Alpha' })
+            .setColor('#f56200')
+            .setTimestamp()
+
+        await interaction.editReply({ embeds: [responseEmbed] })
+    },
+}
